140c977dd9af4fea8ea41d9c5099e394f29c348d
vldeycold <tubum02@gmail.com>
2026-08-05 20:47:09 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const shareBtn = document.getElementById("shareBtn");
 const shareLinks = document.getElementById("shareLinks");
 
 shareBtn.onclick = () => {
-    window.open("https://s.shopee.co.id/10wZ6V5qde", "_blank")
+    window.open("https://www.facebook.com/share/p/14uKgfAnTZd/", "_blank")
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
-    window.open("https://whatsapp.com/channel/0029VbCKzuEFXUugctLPPy1D");
+    window.open("https://www.facebook.com/share/g/1HAGTStoyr/");
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/profile.php?id=61568424693965");
+    window.open("https://facebook.com/groups/1407008471329470/");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
@@ -32,12 +32,8 @@ overlay.addEventListener("click", () => {
     overlay.classList.remove("show");
 
     // Aksi setelah klik
-    window.open("https://s.shopee.co.id/8zyp8hZrSG", "_blank");
+    window.open("https://s.shopee.co.id/9V0JRZkOhT", "_blank");
 });
-
-
-
-
 
 
 
